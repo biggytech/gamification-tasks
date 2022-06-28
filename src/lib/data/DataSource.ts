3a8c4d60b2240062f496ac8e9622550df8c553ad
Neli Harbuzava <ineliharbuzava@gmail.com>
2022-06-28 20:44:30 +0300

    Added datasource class

---
+++ src/lib/data/DataSource.ts
@@ -0,0 +1,37 @@
+import { IDataSource } from '../types';
+
+class DataSource<TData, TActions extends string | number | symbol>
+  implements IDataSource<TData, TActions>
+{
+  private data: TData;
+  private actionHandler: (
+    data: TData,
+    action: TActions,
+    value: any,
+  ) => Promise<TData>;
+  actions: { [action in TActions]: string };
+
+  constructor(
+    initialData: TData,
+    actionHandler: (
+      data: TData,
+      action: TActions,
+      value: any,
+    ) => Promise<TData>,
+    actions: { [action in TActions]: string },
+  ) {
+    this.data = initialData;
+    this.actionHandler = actionHandler;
+    this.actions = actions;
+  }
+
+  getData(): TData {
+    return this.data;
+  }
+
+  async callAction(action: keyof typeof this.actions, value: any) {
+    this.data = await this.actionHandler(this.data, action, value);
+  }
+}
+
+export default DataSource;
