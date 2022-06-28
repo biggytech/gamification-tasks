b5c1dba465da04bbeacf3453948285704077253d
Neli Harbuzava <ineliharbuzava@gmail.com>
2022-06-28 20:44:17 +0300

    Added types

---
+++ src/lib/types.ts
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavigationProp } from '@react-navigation/native';
+
+export interface IModule<TData, TActions extends string | number | symbol> {
+  name: string;
+  Component: React.ComponentType<ModuleComponentProps<TData, TActions>>;
+}
+
+export type Key = number;
+
+export type ModuleComponentProps<
+  TData,
+  TActions extends string | number | symbol,
+> = React.PropsWithoutRef<{}> & {
+  data: TData;
+  callDispatch: (action: TActions, value?: any) => void;
+  actions: { [action in TActions]: TActions };
+  navigation: NavigationProp<any>;
+};
+
+export type ModuleComponent<
+  TData,
+  TActions extends string | number | symbol,
+> = React.ComponentType<ModuleComponentProps<TData, TActions>>;
+
+export interface IDataSource<TData, TActions extends string | number | symbol> {
+  getData: () => TData;
+  callAction: (action: TActions, value?: any) => void;
+  actions: { [action in TActions]: string };
+}
+
+export interface ILabelData {
+  name: string;
+  color: string;
+}
+
+export interface ILabel extends ILabelData {
+  key: Key;
+}
